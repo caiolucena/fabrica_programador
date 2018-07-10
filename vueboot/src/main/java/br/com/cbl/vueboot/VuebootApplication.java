6fa42dde7069cb6fa324f5fe2d6e2ff981d0de09
Caio Lucena <caiolucena.cc@gmail.com>
2018-07-10 09:43:05 -0300

    Primeiro commit do projeto

---
+++ vueboot/src/main/java/br/com/cbl/vueboot/VuebootApplication.java
@@ -0,0 +1,12 @@
+package br.com.cbl.vueboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuebootApplication.class, args);
+	}
+}
